39eb2b77a7a0a7f7a73ee094e14ea599f38d83be
Early <lin_erli@outlook.com>
2020-08-17 19:34:23 +0800

    Init

---
+++ modules/calendar.js
@@ -0,0 +1,28 @@
+app.get('/calendar', async (req, res) => {
+  if (!syzoj.config.calendar.enable) {
+    res.render('error', {
+      err: null
+    });
+  } else {
+    const resp = await fetch(syzoj.config.calendar.apiUrl);
+    const data = await resp.json();
+
+    if (data.status !== 'OK') {
+      res.render('error', {
+        err: data.message
+      });
+    } else {
+      let contests = [];
+      for (let oj of data.oj) {
+        for (let contest of oj.contests) {
+          contest.oj = oj;
+          contests.push(contest);
+        }
+      }
+      contests.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
+      res.render('calendar', {
+        contests: contests
+      });
+    }
+  }
+});
